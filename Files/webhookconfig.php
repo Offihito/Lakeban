9bb9b2a273bee0215d9819f1d81e8a6eb6e1544f
Offihito <gserdar734@gmail.com>
2025-12-24 22:08:39 +0300

    Update webhookconfig.php

--- Files/webhookconfig.php
+++ Files/webhookconfig.php
@@ -1,3 +1,4 @@
 <?php
-define('STRIPE_API_KEY', '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');
-define('STRIPE_WEBHOOK_SECRET', 'whsec_ms3ereEudl035NTh2weYsJAGRPxNtCML');
+define('STRIPE_API_KEY', '');
+
+define('STRIPE_WEBHOOK_SECRET', '');
